a745dc2220427156af18e9b45246d6f76377c841
Andrew O'Grady <andrewogrady3@gmail.com>
2020-01-23 16:16:02 -0500

    added functionaility

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function Open() {
 function Close() {
     document.getElementById("side-navbar").style.width = "0";
 };
+
+function closeProject(){
+    document.getElementsByClassName("project-dropdown")[0].style.height = "0"
+    document.getElementsByClassName("project-dropdown")[1].style.height = "0"
+    document.getElementsByClassName("project-dropdown")[2].style.height = "0"
+}
+
+function openProject(num) {
+    closeProject()
+    document.getElementsByClassName(`${num}`)[0].style.height = "200px"
+    
+};
