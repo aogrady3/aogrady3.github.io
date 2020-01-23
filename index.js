f49878154a7588494b84bb23eec0b67680e482ad
Andrew O'Grady <andrewogrady3@gmail.com>
2020-01-23 16:56:32 -0500

    responsive desgin

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ function closeProject(){
     document.getElementsByClassName("project-dropdown")[0].style.height = "0"
     document.getElementsByClassName("project-dropdown")[1].style.height = "0"
     document.getElementsByClassName("project-dropdown")[2].style.height = "0"
+    document.getElementsByClassName("project-dropdown")[3].style.height = "0"
+    document.getElementsByClassName("project-dropdown")[4].style.height = "0"
+
 }
 
 function openProject(num) {
     closeProject()
-    document.getElementsByClassName(`${num}`)[0].style.height = "200px"
+    document.getElementsByClassName(`${num}`)[0].style.height = "250px"
     
 };
